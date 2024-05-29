533bb7a8e3b4c6e5e507b1bf89458f02288fa615
AbhishekB27 <akstyles27@gmail.com>
2024-05-29 08:05:10 +0530

    added form validation zod,react-hook-form

--- src/components/codeShare/CodeShare.tsx
+++ src/components/codeShare/CodeShare.tsx
@@ -1,16 +1,19 @@
+"use client"
 import { format } from 'prettier';
-import React from 'react'
+import React, { useState } from 'react'
 import { TbCopy } from "react-icons/tb";
 import { MdContentPaste } from "react-icons/md";
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 import { dark } from 'react-syntax-highlighter/dist/esm/styles/prism';
 import { ShareModal } from './ShareModal';
 
-const CodeShare = async ({ code }: { code: string }) => {
+const CodeShare = ({ code }: { code: string }) => {
     // Format the code using Prettier
-    console.log(code)
-    const formattedCode = await format(code, { semi: false, parser: "babel" });
-    console.log(formattedCode)
+    const [codeContent, setCodeContent] = useState('// Paste Your Code...')
+    format(code, { semi: false, parser: "babel" }).then(code => {
+        console.log(code, "code")
+        setCodeContent(code)
+    }).catch(err => console.error(err));
     const customStyle = {
         background: 'none',
         fontFamily: 'inherit',
@@ -23,27 +26,30 @@ const CodeShare = async ({ code }: { code: string }) => {
     };
 
     return (
-        <div className='bg-slate-900 relative max-w-5xl w-full overflow-hidden min-h-[90vh] text-white text-center rounded-md'>
-            <div className='bg-slate-800 min-h-10 p-1 flex justify-between items-center px-3 gap-3'>
+        <form
+            className='bg-slate-900 relative max-w-5xl w-full overflow-hidden min-h-[90vh] text-white text-center rounded-md'>
+            <div className='relative bg-slate-800 min-h-10 p-1 flex justify-between items-center px-3 gap-3'>
                 <div className='flex justify-start items-center gap-3'>
                     <div className='size-3 rounded-full bg-red-600'></div>
                     <div className='size-3 rounded-full bg-yellow-600'></div>
                     <div className='size-3 rounded-full bg-green-600'></div>
                 </div>
-                <div className='flex justify-center items-center gap-4'>
-                    <ShareModal />
-                    <button className='active:bg-black/50 transition-all bg-black/30 p-2 rounded-md' type="button"><TbCopy /></button>
+                <div className='flex justify-center items-center gap-4 z-50'>
+                    <ShareModal codeContent={`${codeContent}`} />
+                    <button className='active:bg-black/50 transition-all bg-black/30 p-2 rounded-md' ><TbCopy /></button>
                     <button className='active:bg-black/50 transition-all bg-black/30 p-2 rounded-md' type="button"><MdContentPaste /></button>
                 </div>
+
             </div>
-            <textarea
-
-                readOnly className='absolute focus:outline-none bg-black/30 w-full h-full resize-none right-0' name="" id=""></textarea>
+            <div
+                onPaste={(e) => setCodeContent(e.clipboardData.getData("Text"))}
+                className='absolute focus:outline-none bg-white/10 w-full h-full resize-none right-0'
+                id="" ></div>
             <SyntaxHighlighter language="jsx" style={dark} customStyle={customStyle}>
-
-                {formattedCode}
+                {codeContent}
+                {/* {formattedCode} */}
             </SyntaxHighlighter>
-        </div>
+        </form>
     )
 }
 
--- src/components/codeShare/ShareModal.tsx
+++ src/components/codeShare/ShareModal.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import { Copy } from "lucide-react"
 
 import { Button } from "@/components/ui/button"
@@ -16,9 +18,40 @@ import { Label } from "@/components/ui/label"
 import { Checkbox } from "../ui/checkbox"
 import { TbCopy } from "react-icons/tb";
 import { FaShareNodes } from "react-icons/fa6";
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "../ui/form"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+import { zodResolver } from "@hookform/resolvers/zod"
+
+const formSchema = z.object({
+    title: z.string().min(2, {
+        message: "Title must be at least 2 characters.",
+    }),
+    link: z.string().min(2, {
+        message: "Link must be at least 2 characters.",
+    }),
+    isPublic: z.boolean().default(false).optional(),
+})
 
 
-export function ShareModal() {
+export function ShareModal({ codeContent }: { codeContent: string }) {
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            link: "https://ui.shadcn.com/docs/installation",
+            isPublic: false
+        },
+    })
+
+
+
+    function onSubmit(values: z.infer<typeof formSchema>) {
+        // Do something with the form values.
+        // ✅ This will be type-safe and validated.
+        console.log(values, "values")
+    }
+
+
     return (
         <Dialog>
             <DialogTrigger asChild>
@@ -31,53 +64,74 @@ export function ShareModal() {
                         Anyone who has this link will be able to view this.
                     </DialogDescription>
                 </DialogHeader>
-                <div className="space-y-4">
-
-                    <div className="flex items-center space-x-2">
-
-                        <div className="grid flex-1 gap-2">
-                            <Label htmlFor="link" className="sr-only">
-                                Link
-                            </Label>
-                            <Input
-                                id="link"
-                                defaultValue="https://ui.shadcn.com/docs/installation"
-                                readOnly
-                            />
-                        </div>
-                        <Button type="submit" size="sm" className="px-3">
-                            <span className="sr-only">Copy</span>
-                            <Copy className="h-4 w-4" />
-                        </Button>
-                    </div>
-                    <div className="grid flex-1 gap-2">
-                        <Label htmlFor="link" className="sr-only">
-                            Link
-                        </Label>
-                        <Input
-                            id="link"
-                            placeholder="Your Title..."
-                            defaultValue=""
-                        // readOnly
+                <Form {...form}>
+                    <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+                        <FormField
+                            control={form.control}
+                            name="link"
+                            render={({ field }) => (
+                                <FormItem>
+                                    {/* <FormLabel>Link</FormLabel> */}
+                                    <div className="flex items-center space-x-2">
+                                        <FormControl>
+                                            <Input readOnly defaultValue='https://ui.shadcn.com/docs/installation' placeholder="shadcn" {...field} />
+                                        </FormControl>
+                                        <Button type="submit" size="sm" className="px-3">
+                                            <span className="sr-only">Copy</span>
+                                            <Copy className="h-4 w-4" />
+                                        </Button>
+                                    </div>
+                                    {/* <FormDescription>
+                                        This is your public display name.
+                                    </FormDescription> */}
+                                    <FormMessage />
+                                </FormItem>
+                            )}
                         />
-                    </div>
-                    <div className="flex items-center space-x-2">
-                        <Checkbox id="terms" />
-                        <label
-                            htmlFor="terms"
-                            className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-                        >
-                            Make a Private
-                        </label>
-                    </div>
-                </div>
-                <DialogFooter className="sm:justify-start">
+                        <FormField
+                            control={form.control}
+                            name="title"
+                            render={({ field }) => (
+                                <FormItem>
+                                    {/* <FormLabel>Title</FormLabel> */}
+                                    <FormControl>
+                                        <Input placeholder="Enter Title Here..." {...field} />
+                                    </FormControl>
+                                    {/* <FormDescription>
+                                        This is your public display name.
+                                    </FormDescription> */}
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                        <FormField
+                            control={form.control}
+                            name="isPublic"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <div className="flex items-center space-x-2">
+                                        <FormControl>
+                                            <Checkbox checked={field.value}
+                                                onCheckedChange={field.onChange} />
+                                        </FormControl>
+                                        <FormLabel>Private</FormLabel>
+                                    </div>
+                                    {/* <FormDescription>
+                                        This is your public display name.
+                                    </FormDescription> */}
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                    </form>
+                </Form>
+                {/* <DialogFooter className="sm:justify-start">
                     <DialogClose asChild>
                         <Button type="button" variant="secondary">
                             Close
                         </Button>
                     </DialogClose>
-                </DialogFooter>
+                </DialogFooter> */}
             </DialogContent>
         </Dialog>
     )
