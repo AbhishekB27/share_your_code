85c027e504a69602ed92c9806e1c96eff92e594a
AbhishekB27 <akstyles27@gmail.com>
2024-08-20 12:46:39 +0530

    fixed auth.ts file

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,26 +1,31 @@
-import mongoose from "mongoose";
+let User;
 
-const userSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  isVerified: {
-    type: Boolean,
-    default: false,
-  },
-  verificationToken: String,
-});
+if (typeof window === "undefined") {
+  const mongoose = require("mongoose");
 
-const User = mongoose.models?.user || mongoose.model("user", userSchema);
+  const userSchema = new mongoose.Schema({
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    isVerified: {
+      type: Boolean,
+      default: false,
+    },
+    verificationToken: String,
+  });
+
+  User = mongoose.models?.user || mongoose.model("user", userSchema);
+}
+
 export default User;
