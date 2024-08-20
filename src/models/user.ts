8ec1484281a3b94d052cbf45c14767d36a5fc153
AbhishekB27 <akstyles27@gmail.com>
2024-08-20 13:01:54 +0530

    user model

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,31 +1,27 @@
-let User;
+import { verify } from "crypto";
+import mongoose from "mongoose";
 
-if (typeof window === "undefined") {
-  const mongoose = require("mongoose");
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  isVerified: {
+    type: Boolean,
+    default: false,
+  },
+  verificationToken: String,
+});
 
-  const userSchema = new mongoose.Schema({
-    username: {
-      type: String,
-      required: true,
-      unique: true,
-    },
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-    },
-    password: {
-      type: String,
-      required: true,
-    },
-    isVerified: {
-      type: Boolean,
-      default: false,
-    },
-    verificationToken: String,
-  });
-
-  User = mongoose.models?.user || mongoose.model("user", userSchema);
-}
-
+const User = mongoose.models?.users || mongoose.model("users", userSchema);
 export default User;
